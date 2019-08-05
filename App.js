5577c419168999696190cf7da9aa0452e460fb96
ndlganeshu <ndlganeswararao@gmail.com>
2019-08-05 21:59:16 +0530

    Added new file

---
+++ App.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((req,res)=>{
+	res.statusCode = 200;
+	res.setHeader('Content-Type','text/plain');
+	res.end( "hello world");
+})
+
+server.listen(3005,'127.0.0.1',()=>{
+	console.log('server running at http://127.0.0.1:3000/')
+})
