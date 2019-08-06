9e7f73b7307f012f6cc51233209fb74155e24d58
ndlganeshu <ndlganeswararao@gmail.com>
2019-08-06 21:25:46 +0530

    Adding the mongodb connection

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 const http = require('http');
+var mongoose = require ("mongoose");
 
 const server = http.createServer((req,res)=>{
 	res.statusCode = 200;
@@ -11,3 +12,16 @@ const port = process.env.PORT || 3000;
 server.listen(port ,host,()=>{
 	console.log('server started.....')
 })
+
+var uristring =
+process.env.MONGOLAB_URI ||
+process.env.MONGOHQ_URL ||
+'mongodb+srv://ganesh:ganeshdb@mytestcluster-ed0df.mongodb.net/test';
+
+mongoose.connect(uristring, function (err, res) {
+    if (err) {
+    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+    } else {
+    console.log ('Succeeded connected to: ' + uristring);
+    }
+  });
