2c102e969645dc29c259272e214be7f8ae430ec1
ndlganeshu <ndlganeswararao@gmail.com>
2019-08-06 21:36:29 +0530

    Testing the mongodb connection

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
-const http = require('http');
+// const http = require('http');
 var mongoose = require ("mongoose");
 
-const server = http.createServer((req,res)=>{
-	res.statusCode = 200;
-	res.setHeader('Content-Type','text/plain');
-	res.end( "hello world");
-})
-const host = '0.0.0.0';
-const port = process.env.PORT || 3000;
+// const server = http.createServer((req,res)=>{
+// 	res.statusCode = 200;
+// 	res.setHeader('Content-Type','text/plain');
+// 	res.end( "hello world");
+// })
+// const host = '0.0.0.0';
+// const port = process.env.PORT || 3000;
 
-server.listen(port ,host,()=>{
-	console.log('server started.....')
-})
+// server.listen(port ,host,()=>{
+// 	console.log('server started.....')
+// })
 
 var uristring =
 process.env.MONGOLAB_URI ||
