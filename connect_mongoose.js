3ba356d01576c384d9471b4070e8801a95dcc4d0
ndlganeshu <ndlganeswararao@gmail.com>
2019-08-06 21:20:23 +0530

    Uploading the mongoose connection

---
+++ connect_mongoose.js
@@ -0,0 +1,15 @@
+var http = require ('http');         // For serving a basic web page.
+var mongoose = require ("mongoose"); // The reason for this demo.
+
+var uristring =
+process.env.MONGOLAB_URI ||
+process.env.MONGOHQ_URL ||
+'mongodb://localhost:27017/';
+
+mongoose.connect(uristring, function (err, res) {
+    if (err) {
+    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+    } else {
+    console.log ('Succeeded connected to: ' + uristring);
+    }
+  });
